f728473cb2525474eb5e4f2eea4a6c7442a98fed
Khaled Moataz <khaledmoataz1999@yahoo.com>
2018-12-14 19:17:05 +0200

    adding accepted to enroll table

--- photofia.sql
+++ photofia.sql
@@ -2,10 +2,10 @@
 -- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Dec 14, 2018 at 04:28 PM
--- Server version: 5.7.23
--- PHP Version: 7.2.10
+-- Host: 127.0.0.1
+-- Generation Time: Dec 14, 2018 at 06:15 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,8 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `photofia`
 --
-CREATE DATABASE IF NOT EXISTS `photofia` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `photofia`;
 
 -- --------------------------------------------------------
 
@@ -30,13 +28,10 @@ USE `photofia`;
 -- Table structure for table `awarded`
 --
 
-DROP TABLE IF EXISTS `awarded`;
-CREATE TABLE IF NOT EXISTS `awarded` (
+CREATE TABLE `awarded` (
   `awardName` varchar(50) NOT NULL,
   `customerEmail` varchar(50) NOT NULL,
-  `quantity` int(11) DEFAULT NULL,
-  PRIMARY KEY (`awardName`,`customerEmail`),
-  KEY `customerEmail` (`customerEmail`)
+  `quantity` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -45,15 +40,13 @@ CREATE TABLE IF NOT EXISTS `awarded` (
 -- Table structure for table `camera`
 --
 
-DROP TABLE IF EXISTS `camera`;
-CREATE TABLE IF NOT EXISTS `camera` (
+CREATE TABLE `camera` (
   `cameraName` varchar(50) NOT NULL,
   `sensorSize` varchar(50) DEFAULT NULL,
   `brand` varchar(50) DEFAULT NULL,
   `resolution` varchar(50) DEFAULT NULL,
   `videoCaptureResolution` varchar(50) DEFAULT NULL,
-  `opticalZoom` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`cameraName`)
+  `opticalZoom` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -62,11 +55,9 @@ CREATE TABLE IF NOT EXISTS `camera` (
 -- Table structure for table `customer`
 --
 
-DROP TABLE IF EXISTS `customer`;
-CREATE TABLE IF NOT EXISTS `customer` (
+CREATE TABLE `customer` (
   `customerEmail` varchar(50) NOT NULL,
-  `points` int(11) DEFAULT NULL,
-  PRIMARY KEY (`customerEmail`)
+  `points` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -75,10 +66,8 @@ CREATE TABLE IF NOT EXISTS `customer` (
 -- Table structure for table `device`
 --
 
-DROP TABLE IF EXISTS `device`;
-CREATE TABLE IF NOT EXISTS `device` (
-  `deviceName` varchar(50) NOT NULL,
-  PRIMARY KEY (`deviceName`)
+CREATE TABLE `device` (
+  `deviceName` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -87,12 +76,10 @@ CREATE TABLE IF NOT EXISTS `device` (
 -- Table structure for table `enroll`
 --
 
-DROP TABLE IF EXISTS `enroll`;
-CREATE TABLE IF NOT EXISTS `enroll` (
+CREATE TABLE `enroll` (
   `id` int(11) NOT NULL,
   `photographerEmail` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`,`photographerEmail`),
-  KEY `photographerEmail` (`photographerEmail`)
+  `accepted` bit(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -101,16 +88,12 @@ CREATE TABLE IF NOT EXISTS `enroll` (
 -- Table structure for table `events`
 --
 
-DROP TABLE IF EXISTS `events`;
-CREATE TABLE IF NOT EXISTS `events` (
+CREATE TABLE `events` (
   `userEmail` varchar(50) DEFAULT NULL,
   `date` date DEFAULT NULL,
   `description` varchar(1000) DEFAULT NULL,
   `id` int(11) NOT NULL,
-  `eventLocation` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `userEmail` (`userEmail`),
-  KEY `eventLocation` (`eventLocation`)
+  `eventLocation` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -119,12 +102,9 @@ CREATE TABLE IF NOT EXISTS `events` (
 -- Table structure for table `follow`
 --
 
-DROP TABLE IF EXISTS `follow`;
-CREATE TABLE IF NOT EXISTS `follow` (
+CREATE TABLE `follow` (
   `userEmail` varchar(50) NOT NULL,
-  `photographerEmail` varchar(50) NOT NULL,
-  PRIMARY KEY (`photographerEmail`,`userEmail`),
-  KEY `userEmail` (`userEmail`)
+  `photographerEmail` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -133,12 +113,10 @@ CREATE TABLE IF NOT EXISTS `follow` (
 -- Table structure for table `gift`
 --
 
-DROP TABLE IF EXISTS `gift`;
-CREATE TABLE IF NOT EXISTS `gift` (
+CREATE TABLE `gift` (
   `giftName` varchar(50) NOT NULL,
   `expirationDate` date DEFAULT NULL,
-  `points` int(11) DEFAULT NULL,
-  PRIMARY KEY (`giftName`)
+  `points` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -147,18 +125,14 @@ CREATE TABLE IF NOT EXISTS `gift` (
 -- Table structure for table `image`
 --
 
-DROP TABLE IF EXISTS `image`;
-CREATE TABLE IF NOT EXISTS `image` (
+CREATE TABLE `image` (
   `path` varchar(10) NOT NULL,
   `type` varchar(50) DEFAULT NULL,
   `rate` float DEFAULT NULL,
   `description` varchar(100) DEFAULT NULL,
   `date` date DEFAULT NULL,
   `photographerEmail` varchar(50) DEFAULT NULL,
-  `imageLocation` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`path`),
-  KEY `photographerEmail` (`photographerEmail`),
-  KEY `imageLocation` (`imageLocation`)
+  `imageLocation` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -174,15 +148,13 @@ INSERT INTO `image` (`path`, `type`, `rate`, `description`, `date`, `photographe
 -- Table structure for table `lens`
 --
 
-DROP TABLE IF EXISTS `lens`;
-CREATE TABLE IF NOT EXISTS `lens` (
+CREATE TABLE `lens` (
   `lensName` varchar(50) NOT NULL,
   `lensType` varchar(50) DEFAULT NULL,
   `focusType` varchar(50) DEFAULT NULL,
   `minFocalRange` int(11) DEFAULT NULL,
   `maxFocalRange` int(11) DEFAULT NULL,
-  `maxApretureRange` int(11) DEFAULT NULL,
-  PRIMARY KEY (`lensName`)
+  `maxApretureRange` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -191,14 +163,12 @@ CREATE TABLE IF NOT EXISTS `lens` (
 -- Table structure for table `orders`
 --
 
-DROP TABLE IF EXISTS `orders`;
-CREATE TABLE IF NOT EXISTS `orders` (
+CREATE TABLE `orders` (
   `photographerEmail` varchar(50) NOT NULL,
   `customerEmail` varchar(50) NOT NULL,
-  `requestedTime` datetime NOT NULL,
-  `accepted` bit(1) DEFAULT NULL,
-  PRIMARY KEY (`photographerEmail`,`customerEmail`,`requestedTime`),
-  KEY `customerEmail` (`customerEmail`)
+  `orderCode` int(11) NOT NULL,
+  `orderDay` int(11) NOT NULL,
+  `accepted` bit(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -207,12 +177,9 @@ CREATE TABLE IF NOT EXISTS `orders` (
 -- Table structure for table `owns`
 --
 
-DROP TABLE IF EXISTS `owns`;
-CREATE TABLE IF NOT EXISTS `owns` (
+CREATE TABLE `owns` (
   `deviceName` varchar(50) NOT NULL,
-  `photographerEmail` varchar(50) NOT NULL,
-  PRIMARY KEY (`deviceName`,`photographerEmail`),
-  KEY `photographerEmail` (`photographerEmail`)
+  `photographerEmail` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -221,14 +188,12 @@ CREATE TABLE IF NOT EXISTS `owns` (
 -- Table structure for table `photographer`
 --
 
-DROP TABLE IF EXISTS `photographer`;
-CREATE TABLE IF NOT EXISTS `photographer` (
+CREATE TABLE `photographer` (
   `bio` varchar(1000) DEFAULT NULL,
   `avgPrice` int(11) DEFAULT NULL,
   `photographerEmail` varchar(50) NOT NULL,
   `qualifications` varchar(1000) DEFAULT NULL,
-  `rate` float DEFAULT NULL,
-  PRIMARY KEY (`photographerEmail`)
+  `rate` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -244,12 +209,10 @@ INSERT INTO `photographer` (`bio`, `avgPrice`, `photographerEmail`, `qualificati
 -- Table structure for table `place`
 --
 
-DROP TABLE IF EXISTS `place`;
-CREATE TABLE IF NOT EXISTS `place` (
+CREATE TABLE `place` (
   `placeName` varchar(50) NOT NULL,
   `description` varchar(1000) DEFAULT NULL,
-  `placePhone` varchar(11) DEFAULT NULL,
-  PRIMARY KEY (`placeName`)
+  `placePhone` varchar(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -265,30 +228,24 @@ INSERT INTO `place` (`placeName`, `description`, `placePhone`) VALUES
 -- Table structure for table `reviewimage`
 --
 
-DROP TABLE IF EXISTS `reviewimage`;
-CREATE TABLE IF NOT EXISTS `reviewimage` (
+CREATE TABLE `reviewimage` (
   `userEmail` varchar(50) NOT NULL,
   `path` varchar(10) NOT NULL,
   `comment` varchar(100) DEFAULT NULL,
-  `rate` float DEFAULT NULL,
-  PRIMARY KEY (`userEmail`,`path`),
-  KEY `path` (`path`)
+  `rate` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Triggers `reviewimage`
 --
-DROP TRIGGER IF EXISTS `update_image_rate_after_delete`;
 DELIMITER $$
 CREATE TRIGGER `update_image_rate_after_delete` AFTER DELETE ON `reviewimage` FOR EACH ROW UPDATE image SET rate = (SELECT AVG(rate) FROM reviewimage where path = OLD.path) WHERE path = OLD.path
 $$
 DELIMITER ;
-DROP TRIGGER IF EXISTS `update_image_rate_after_insert`;
 DELIMITER $$
 CREATE TRIGGER `update_image_rate_after_insert` AFTER INSERT ON `reviewimage` FOR EACH ROW UPDATE image SET rate = (SELECT AVG(rate) FROM reviewimage where path = NEW.path) WHERE path = NEW.path
 $$
 DELIMITER ;
-DROP TRIGGER IF EXISTS `update_image_rate_after_update`;
 DELIMITER $$
 CREATE TRIGGER `update_image_rate_after_update` AFTER UPDATE ON `reviewimage` FOR EACH ROW UPDATE image SET rate = (SELECT AVG(rate) FROM reviewimage where path = NEW.path) WHERE path = NEW.path
 $$
@@ -300,30 +257,24 @@ DELIMITER ;
 -- Table structure for table `reviewphotographer`
 --
 
-DROP TABLE IF EXISTS `reviewphotographer`;
-CREATE TABLE IF NOT EXISTS `reviewphotographer` (
+CREATE TABLE `reviewphotographer` (
   `photographerEmail` varchar(50) NOT NULL,
   `customerEmail` varchar(50) NOT NULL,
   `comment` varchar(100) DEFAULT NULL,
-  `rate` float DEFAULT NULL,
-  PRIMARY KEY (`photographerEmail`,`customerEmail`),
-  KEY `customerEmail` (`customerEmail`)
+  `rate` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Triggers `reviewphotographer`
 --
-DROP TRIGGER IF EXISTS `update_photographer_rate_after_delete`;
 DELIMITER $$
 CREATE TRIGGER `update_photographer_rate_after_delete` AFTER DELETE ON `reviewphotographer` FOR EACH ROW UPDATE photographer SET rate = (SELECT AVG(rate) FROM reviewphotographer WHERE photographerEmail = OLD.photographerEmail) WHERE photographerEmail = OLD.photographerEmail
 $$
 DELIMITER ;
-DROP TRIGGER IF EXISTS `update_photographer_rate_after_install`;
 DELIMITER $$
 CREATE TRIGGER `update_photographer_rate_after_install` AFTER INSERT ON `reviewphotographer` FOR EACH ROW UPDATE photographer SET rate = (SELECT AVG(rate) FROM reviewphotographer WHERE photographerEmail = NEW.photographerEmail) WHERE photographerEmail = NEW.photographerEmail
 $$
 DELIMITER ;
-DROP TRIGGER IF EXISTS `update_photographer_rate_after_update`;
 DELIMITER $$
 CREATE TRIGGER `update_photographer_rate_after_update` AFTER UPDATE ON `reviewphotographer` FOR EACH ROW UPDATE photographer SET rate = (SELECT AVG(rate) FROM reviewphotographer WHERE photographerEmail = NEW.photographerEmail) WHERE photographerEmail = NEW.photographerEmail
 $$
@@ -335,14 +286,11 @@ DELIMITER ;
 -- Table structure for table `reviewplace`
 --
 
-DROP TABLE IF EXISTS `reviewplace`;
-CREATE TABLE IF NOT EXISTS `reviewplace` (
+CREATE TABLE `reviewplace` (
   `userEmail` varchar(50) NOT NULL,
   `placeName` varchar(50) NOT NULL,
   `comment` varchar(100) DEFAULT NULL,
-  `rate` float DEFAULT NULL,
-  PRIMARY KEY (`userEmail`,`placeName`),
-  KEY `placeName` (`placeName`)
+  `rate` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -351,13 +299,10 @@ CREATE TABLE IF NOT EXISTS `reviewplace` (
 -- Table structure for table `schedule`
 --
 
-DROP TABLE IF EXISTS `schedule`;
-CREATE TABLE IF NOT EXISTS `schedule` (
+CREATE TABLE `schedule` (
   `day` int(11) NOT NULL,
   `photographerEmail` varchar(50) NOT NULL,
-  `code` int(11) DEFAULT NULL,
-  PRIMARY KEY (`day`,`photographerEmail`),
-  KEY `photographerEmail` (`photographerEmail`)
+  `code` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -366,8 +311,7 @@ CREATE TABLE IF NOT EXISTS `schedule` (
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `userEmail` varchar(50) NOT NULL,
   `username` varchar(50) NOT NULL,
   `userPhone` varchar(11) DEFAULT NULL,
@@ -376,8 +320,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `gender` bit(1) DEFAULT NULL,
   `birthDate` date DEFAULT NULL,
   `profilePicture` varchar(10) DEFAULT NULL,
-  `privilege` int(2) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`userEmail`)
+  `privilege` int(2) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -386,6 +329,138 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`userEmail`, `username`, `userPhone`, `password`, `userAddress`, `gender`, `birthDate`, `profilePicture`, `privilege`) VALUES
 ('ayman.elakwah@gmail.com', 'Ayman', '01100110011', 'password', 'october', b'0', '2018-12-04', NULL, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `awarded`
+--
+ALTER TABLE `awarded`
+  ADD PRIMARY KEY (`awardName`,`customerEmail`),
+  ADD KEY `customerEmail` (`customerEmail`);
+
+--
+-- Indexes for table `camera`
+--
+ALTER TABLE `camera`
+  ADD PRIMARY KEY (`cameraName`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`customerEmail`);
+
+--
+-- Indexes for table `device`
+--
+ALTER TABLE `device`
+  ADD PRIMARY KEY (`deviceName`);
+
+--
+-- Indexes for table `enroll`
+--
+ALTER TABLE `enroll`
+  ADD PRIMARY KEY (`id`,`photographerEmail`),
+  ADD KEY `photographerEmail` (`photographerEmail`);
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userEmail` (`userEmail`),
+  ADD KEY `eventLocation` (`eventLocation`);
+
+--
+-- Indexes for table `follow`
+--
+ALTER TABLE `follow`
+  ADD PRIMARY KEY (`photographerEmail`,`userEmail`),
+  ADD KEY `userEmail` (`userEmail`);
+
+--
+-- Indexes for table `gift`
+--
+ALTER TABLE `gift`
+  ADD PRIMARY KEY (`giftName`);
+
+--
+-- Indexes for table `image`
+--
+ALTER TABLE `image`
+  ADD PRIMARY KEY (`path`),
+  ADD KEY `photographerEmail` (`photographerEmail`),
+  ADD KEY `imageLocation` (`imageLocation`);
+
+--
+-- Indexes for table `lens`
+--
+ALTER TABLE `lens`
+  ADD PRIMARY KEY (`lensName`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`photographerEmail`,`customerEmail`,`orderCode`,`orderDay`),
+  ADD KEY `orders_ibfk_2` (`customerEmail`),
+  ADD KEY `orders_ibfk_3` (`orderDay`);
+
+--
+-- Indexes for table `owns`
+--
+ALTER TABLE `owns`
+  ADD PRIMARY KEY (`deviceName`,`photographerEmail`),
+  ADD KEY `photographerEmail` (`photographerEmail`);
+
+--
+-- Indexes for table `photographer`
+--
+ALTER TABLE `photographer`
+  ADD PRIMARY KEY (`photographerEmail`);
+
+--
+-- Indexes for table `place`
+--
+ALTER TABLE `place`
+  ADD PRIMARY KEY (`placeName`);
+
+--
+-- Indexes for table `reviewimage`
+--
+ALTER TABLE `reviewimage`
+  ADD PRIMARY KEY (`userEmail`,`path`),
+  ADD KEY `path` (`path`);
+
+--
+-- Indexes for table `reviewphotographer`
+--
+ALTER TABLE `reviewphotographer`
+  ADD PRIMARY KEY (`photographerEmail`,`customerEmail`),
+  ADD KEY `customerEmail` (`customerEmail`);
+
+--
+-- Indexes for table `reviewplace`
+--
+ALTER TABLE `reviewplace`
+  ADD PRIMARY KEY (`userEmail`,`placeName`),
+  ADD KEY `placeName` (`placeName`);
+
+--
+-- Indexes for table `schedule`
+--
+ALTER TABLE `schedule`
+  ADD PRIMARY KEY (`day`,`photographerEmail`),
+  ADD KEY `photographerEmail` (`photographerEmail`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userEmail`);
 
 --
 -- Constraints for dumped tables
@@ -448,8 +523,9 @@ ALTER TABLE `lens`
 -- Constraints for table `orders`
 --
 ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`photographerEmail`) REFERENCES `photographer` (`photographerEmail`),
-  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`customerEmail`) REFERENCES `customer` (`customerEmail`);
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`photographerEmail`) REFERENCES `schedule` (`photographerEmail`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`customerEmail`) REFERENCES `customer` (`customerEmail`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`orderDay`) REFERENCES `schedule` (`day`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `owns`
